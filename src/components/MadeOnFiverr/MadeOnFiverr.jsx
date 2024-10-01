b9e86bedd679097c4050a1245cc2e09c19029600
HieuNghia2k1 <boybin1707@gmail.com>
2024-10-01 23:38:50 +0700

    Fix img 2

--- src/components/MadeOnFiverr/MadeOnFiverr.jsx
+++ src/components/MadeOnFiverr/MadeOnFiverr.jsx
@@ -6,55 +6,55 @@ const MadeOnFiverr = () => {
       <h2 className="text-4xl font-semibold">Made on Fiverr </h2>
       <div className="mt-8 grid grid-cols-4">
         <div className="frist">
-          <img src="../../../public/M_1.jpg" alt="" />
+          <img src="./M_1.jpg" alt="" />
         </div>
         <div className="two">
-          <img src="../../../public/M_1.2.jpg" alt="" />
+          <img src="./M_1.2.jpg" alt="" />
         </div>
         <div className="three">
-          <img src="../../../public/M_1.3.jpg" alt="" />
+          <img src="./M_1.3.jpg" alt="" />
         </div>
         <div className="four">
-          <img src="../../../public/M_1.4.jpg" alt="" />
+          <img src="./M_1.4.jpg" alt="" />
         </div>
         {/* Row_2 */}
         <div className="frist">
-          <img src="../../../public/M_2.jpeg" alt="" />
+          <img src="./M_2.jpeg" alt="" />
         </div>
         <div className="two">
-          <img src="../../../public/M_2.1.jpg" alt="" />
+          <img src="./M_2.1.jpg" alt="" />
         </div>
         <div className="three">
-          <img src="../../../public/M_2.2.jpg" alt="" />
+          <img src="./M_2.2.jpg" alt="" />
         </div>
         <div className="four">
-          <img src="../../../public/M_2.3.jpeg" alt="" />
+          <img src="./M_2.3.jpeg" alt="" />
         </div>
         {/* Row_3 */}
         <div className="frist">
-          <img src="../../../public/M_3.jpeg" alt="" />
+          <img src="./M_3.jpeg" alt="" />
         </div>
         <div className="two">
-          <img src="../../../public/M_3.1.jpg" alt="" />
+          <img src="./M_3.1.jpg" alt="" />
         </div>
         <div className="three">
-          <img src="../../../public/M_3.2.jpg" alt="" />
+          <img src="./M_3.2.jpg" alt="" />
         </div>
         <div className="four">
-          <img src="../../../public/M_3.3.jpg" alt="" />
+          <img src="./M_3.3.jpg" alt="" />
         </div>
         {/* Row_4 */}
         <div className="frist">
-          <img src="../../../public/M_4.jpeg" alt="" />
+          <img src="./M_4.jpeg" alt="" />
         </div>
         <div className="frist">
-          <img src="../../../public/M_4.1.j  peg" alt="" />
+          <img src="./M_4.1.j  peg" alt="" />
         </div>
         <div className="frist">
-          <img src="../../../public/M_4.2.jpg" alt="" />
+          <img src="./M_4.2.jpg" alt="" />
         </div>
         <div className="frist">
-          <img src="../../../public/M_5.jpg" alt="" />
+          <img src="./M_5.jpg" alt="" />
         </div>
       </div>
     </div>
